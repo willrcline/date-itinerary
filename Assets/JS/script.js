7e4190f1f039824236c1c1b6e69e39d9b1afe1d8
Danny Mak <danny.mak2@gmail.com>
2023-04-17 14:07:25 -0500

    added new dropdown menus to specify specifics

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,13 +1,34 @@
-// var themeButtonEl = $('#theme-btn');
+var eventTypeSelect = document.getElementById("event-type");
+var cuisineDropdown = document.getElementById("cuisine-dropdown");
+var sportTypeDropdown = document.getElementById("sport-type-dropdown");
+var musicGenreDropdown = document.getElementById("music-genre-dropdown");
+var movieGenreDropdown = document.getElementById("movie-genre-dropdown");
 
-// var isDark = true;
-
-// themeButtonEl.on('click', function () {
-//     if (isDark) {
-//         $('body').css({ 'background-color': '#ffffff', color: '#1a1a1a' });
-//         isDark = !isDark;
-//     } else {
-//         $('body').css({ 'background-color': '#1a1a1a', color: '#ffffff' });
-//         isDark = !isDark;
-//     }
-// });
+eventTypeSelect.addEventListener("change", function () {
+    if (eventTypeSelect.value == "Food & Drinks") {
+        cuisineDropdown.style.display = "block";
+        sportTypeDropdown.style.display = "none";
+        musicGenreDropdown.style.display = "none";
+        movieGenreDropdown.style.display = "none";
+    } else if (eventTypeSelect.value == "Music Events") {
+        cuisineDropdown.style.display = "none";
+        sportTypeDropdown.style.display = "none";
+        musicGenreDropdown.style.display = "block";
+        movieGenreDropdown.style.display = "none";
+    } else if (eventTypeSelect.value == "Sporting Events") {
+        cuisineDropdown.style.display = "none";
+        sportTypeDropdown.style.display = "block";
+        musicGenreDropdown.style.display = "none";
+        movieGenreDropdown.style.display = "none";
+    } else if (eventTypeSelect.value == "Movie Theatre") {
+        cuisineDropdown.style.display = "none";
+        sportTypeDropdown.style.display = "none";
+        musicGenreDropdown.style.display = "none";
+        movieGenreDropdown.style.display = "block";
+    } else {
+        cuisineDropdown.style.display = "none";
+        sportTypeDropdown.style.display = "none";
+        musicGenreDropdown.style.display = "none";
+        movieGenreDropdown.style.display = "none";
+    }
+});
