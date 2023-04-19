8f047d1f223b2d6a36f14fa82f84628d50aea638
willcline613 <willrcline.atx@gmail.com>
2023-04-19 14:49:59 -0500

    test.js file added. TestAPICalls function works with dummy data added. CallOpenAIAPI function works with dummy data. The two work together. CallOpenAiAPI was put within the .then callEventAPI to prevent an issue of callOpenAIAPI being called before event api had finished. BThis allows them both to run together, but prevents callEventAPI from being run multiple times prior to callOpenAIAPI being called. Any ideas? Unless we find a fix, we will only be able to have one event be added which would make the add event to list button useless. Other ToDos which are commented in the code: 1. make the itinerary display have new lines in it. When console logged, the promptResponse has newlines properly but they are not present when .text() puts it in the html elemnent. 2.instead of just pushing subevent, append event and subevent together to push to list for more precise callEventAPI search. 3. (no ToDo added) add functionality to save event links and time and venue and render this data in another diver.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,7 +1,7 @@
 import { OpenAIAPIKey } from "./config.js"
 
-var itineraryList = []
-var realEvents = []
+export var itineraryList = []
+export var realEvents = []
 var subEvent
 
 var eventTypeSelect = document.getElementById("event-type");
@@ -56,7 +56,7 @@ eventTypeSelect.addEventListener("change", function () {
 //  -d gl="us" \
 //  -d api_key="b2d18364dc288147e06aee5c96e4c16301319c027008e008f003783b10527837"
 
-function callEventAPI(event, itineraryInputs) {
+export function callEventAPI(event, itineraryInputs) {
     const API_KEY = 'b2d18364dc288147e06aee5c96e4c16301319c027008e008f003783b10527837';
     const QUERY = event + " in the " + itineraryInputs.timeOfDay + " on " + itineraryInputs.date;
     const ENGINE = 'google_events';
@@ -74,10 +74,10 @@ function callEventAPI(event, itineraryInputs) {
         return response.json();
     })
     .then((data) => {
-        console.log(data)
         var firstEventResult = data.events_results[0]
         console.log(firstEventResult)
         realEvents.push(firstEventResult)
+        callOpenAIAPI(createPromptForOpenAIAPI(itineraryInputs.location, itineraryInputs.calendarDay, itineraryInputs.date))
         return firstEventResult
         //.description
         //.title
@@ -85,8 +85,9 @@ function callEventAPI(event, itineraryInputs) {
     .catch((error) => {
         console.error('There was a problem with the fetch operation:', error);
     });
+}
 
-function callOpenAIAPI(prompt) {
+export function callOpenAIAPI(prompt) {
     const url = 'https://api.openai.com/v1/chat/completions';
 
     const data = {
@@ -106,6 +107,7 @@ function callOpenAIAPI(prompt) {
         .then((response) => response.json())
         .then((result) => {
             var promptResponse = result.choices[0].message.content
+            console.log(promptResponse)
             var pEl = $('#generate-itinerary')
             pEl.text(promptResponse)
         })
@@ -115,7 +117,7 @@ function callOpenAIAPI(prompt) {
 }
 
 
-function createPromptForOpenAIAPI(location, calendarDay, timeOfDay) {
+export function createPromptForOpenAIAPI(location, calendarDay, timeOfDay) {
     var prompt =
         "The following is an itinerary for a very romantic date night with the user's partner for tonight." + "\n\
     Date Location: " + location + "\n\
@@ -125,12 +127,11 @@ function createPromptForOpenAIAPI(location, calendarDay, timeOfDay) {
 
     for (var i = 0; i < realEvents.length; i++) {
         var realEvent = realEvents[i];
-        //TODO: Change the wording to not say event 1 and event 2
         prompt += "Event title: " + realEvent.title + "\n" + "Event Venue: " + realEvent.venue.name + "\n" + "Event Description: " + realEvent.description + "\n"
     }
 
     prompt += "The itinerary is displayed in a format like this:" + "\n\
-    6:00 PM - Start the night" + "\n\
+    6:00 PM - Start the night" + "\n\n\
     Begin your romantic date night by meeting your partner at a picturesque location, such as the Lady Bird Lake Boardwalk or the Zilker Botanical Garden. Take a leisurely stroll, hand-in-hand, and enjoy each other's company surrounded by nature."
 
     return prompt
@@ -142,6 +143,7 @@ function handleAddToItineraryButton() {
     //.push input to list
     var eventTypeInput = $('#event-type').val();
     if (!itineraryList.includes(eventTypeInput)) {
+        //ToDo: instead of just pushing subEvent to a list, append both the event and sub event concatenated to that list like "subEvent + " " + event" (ie "baseball sporting events")
         itineraryList.push(subEvent);
         //append to screen
         //clearSection(“#itinerary-list”)
@@ -158,7 +160,7 @@ function renderItineraryList() {
     }
 }
 
-function handleSearchButton(e) {
+export function handleSearchButton(e) {
     e.preventDefault()
 
     var locationInput = $("#location").val()
